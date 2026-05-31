cba4772ae5309a73bbeadbf31814bf3efe519cdc
Rahul Kochhar <rkochhar@ea.com>
2026-05-31 09:58:12 -0500

    new commit

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@
 const GOOGLE_CLIENT_ID_KEY = 'alphaagent_google_client_id';
 const GOOGLE_USER_KEY = 'alphaagent_google_user';
 const GOOGLE_ID_TOKEN_KEY = 'alphaagent_google_id_token';
-const SHEETS_BACKEND_URL = ''; // set this to your deployed Google Apps Script Web App endpoint URL (not the spreadsheet URL)
+const SHEETS_BACKEND_URL = 'https://script.google.com/macros/s/AKfycbzb7wUo5vBvaLlxhuLITP_-IkD_c5_a6BdcGwAQ3ileXdnbGpgq1EyXLg_Ll51CJ0PV/exec'; // set this to your deployed Google Apps Script Web App endpoint URL (not the spreadsheet URL)
 const ALLOWED_EMAILS = [
   'rkochhar@gmail.com',
   'rekha.kochhar@gmail.com'
