025a4a77c509d9b09852b0b5ee7c4e546dd1ddcb
Rahul Kochhar <rkochhar@ea.com>
2026-05-31 09:46:49 -0500

    new changes

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@
 const GOOGLE_CLIENT_ID_KEY = 'alphaagent_google_client_id';
 const GOOGLE_USER_KEY = 'alphaagent_google_user';
 const GOOGLE_ID_TOKEN_KEY = 'alphaagent_google_id_token';
-const SHEETS_BACKEND_URL = 'https://docs.google.com/spreadsheets/d/1K3vN0-CFO59WbLOJ2_Jka-PULCbWlMQrQILqvBkae_E/edit?gid=0#gid=0'; // set this to your deployed Google Apps Script endpoint URL
+const SHEETS_BACKEND_URL = ''; // set this to your deployed Google Apps Script Web App endpoint URL (not the spreadsheet URL)
 const ALLOWED_EMAILS = [
   'rkochhar@gmail.com',
   'rekha.kochhar@gmail.com'
